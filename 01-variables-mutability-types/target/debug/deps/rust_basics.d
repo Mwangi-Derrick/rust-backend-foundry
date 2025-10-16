5efdfdded4b18865d311ec86c2b8b1c1092231b2
= <mwangiderrick640@gmail.com>
2025-10-16 20:05:21 +0300

    into lesson 5

---
+++ 01-variables-mutability-types/target/debug/deps/rust_basics.d
@@ -0,0 +1,5 @@
+C:\Users\user\documents\projects\rust_basics\target\debug\deps\rust_basics.d: src\main.rs
+
+C:\Users\user\documents\projects\rust_basics\target\debug\deps\rust_basics.exe: src\main.rs
+
+src\main.rs:
---
+++ 01-variables-mutability-types/target/debug/rust_basics.d
@@ -0,0 +1,1 @@
+C:\Users\user\documents\projects\rust_basics\target\debug\rust_basics.exe: C:\Users\user\documents\projects\rust_basics\src\main.rs
